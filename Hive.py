e98208db5e82c5cdfba5c0cb829fe0df1b2f8166
DaDumples <thehandsomedadumples@gmail.com>
2019-03-17 00:43:27 -0700

    Queen Implemented

--- Hive.py
+++ Hive.py
@@ -4,12 +4,85 @@ from numpy import *
 
 win_size = (1080,720)
 
+def adjacent_tiles(pos):
+	x = pos[0]
+	y = pos[1]
+	return [(x+1, y),
+		    (x-1, y),
+		    (x,   y+1),
+		    (x,   y-1),
+		    (x-1, y+1),
+		    (x+1, y-1)]
+
+def can_squeeze(p1,p2,state):
+	direction = (p2[0] - p1[0], p2[1] - p1[1])
+	if direction == (1,0):
+		left = (p1[0]+0,p1[1]+1)
+		right = (p1[0]+1,p1[1]-1)
+		if (left in state) and (right in state):
+			return False
+	elif direction == (1,-1):
+		left = (p1[0]+1,p1[1]+0)
+		right = (p1[0]+0,p1[1]-1)
+		if (left in state) and (right in state):
+			return False
+	elif direction == (0,-1):
+		left = (p1[0]+1,p1[1]-1)
+		right = (p1[0]-1,p1[1]+0)
+		if (left in state) and (right in state):
+			return False
+	elif direction == (-1,-0):
+		left = (p1[0]+0,p1[1]-1)
+		right = (p1[0]-1,p1[1]+1)
+		if (left in state) and (right in state):
+			return False
+	elif direction == (-1,1):
+		left = (p1[0]-1,p1[1]+0)
+		right = (p1[0]+0,p1[1]+1)
+		if (left in state) and (right in state):
+			return False
+	elif direction == (0,1):
+		left = (p1[0]-1,p1[1]+1)
+		right = (p1[0]+1,p1[1]+0)
+		if (left in state) and (right in state):
+			return False
+	else:
+		print('can_squeeze takes two adjacent points bro.')
+		print('These are at a distance of: '+str(direction))
+		return False
+	return True
+
+def is_contiguous(state):
+	seed = state[0] #random starting tile
+	contiguous_tiles = [seed] #a list of all tiles that are touching the starting tile
+	new_tiles = [] #tiles on the edge of the search
+	for tile in adjacent_tiles(seed):
+		if tile in state: #for every existing tile adjacent to the seed
+			new_tiles.append(tile) #add the adjacent tile to the search
+			contiguous_tiles.append(tile) #add the adjacent tile to the list of touching tiles
+
+	while len(new_tiles) != 0: #until there are no tiles left to check
+		for tile in new_tiles: #for each tile to check
+			for adj_tile in adjacent_tiles(tile): #check each tile adjacent to it
+				if (adj_tile in state) and (adj_tile not in contiguous_tiles): #if it is filled and not already counted
+					new_tiles.append(adj_tile) #add it to the search pile
+					contiguous_tiles.append(adj_tile) #add it to the total touching tiles
+			new_tiles.remove(tile) #Now that it is searched remove this tile from the search pool
+
+	if len(contiguous_tiles) == len(state): #if we didnt find all the tiles in our search, there must be a gap
+		return True
+	else:
+		return False
+
+
+
 class Hex():
 
-	def __init__(self, x, y, screen, im_file = None, color = (180,180,180), linewidth = 0):
-		self.x = x
-		self.y = y
-		self.z = -(x+y) #for whenever you want to use a three axis coordinate system
+	def __init__(self, pos, screen, im_file = None, color = (180,180,180), linewidth = 0):
+		self.x = pos[0]
+		self.y = pos[1]
+		self.pos = pos
+		self.z = -(pos[0] + pos[1]) #for whenever you want to use a three axis coordinate system
 		self.r = win_size[1]*.95/22 #radius is so that 22 hexes can
 									#fit on the screen with some margin
 									#r is the apothem of the hexagon
@@ -40,7 +113,7 @@ class Hex():
 
 class Queen(Hex):
 
-	def __init__(self, player, x, y, screen):
+	def __init__(self, player, pos, screen):
 		if player == 1:
 			color = (210,180,140)
 			linewidth = 0
@@ -48,56 +121,81 @@ class Queen(Hex):
 			color = (25,25,25)
 			linewidth = 0
 		self.player = player
-		Hex.__init__(self, x, y, screen, 'bee.png',color, linewidth)
-
-	def can_move(self, pieces):
-		pass
+		Hex.__init__(self, pos, screen, 'bee.png', color, linewidth)
 
 	def valid_moves(self, pieces):
-		pass
+
+		open_tiles = [] #all the tiles that you could place the tile
+		accessible_tiles = [] #all the tiles that you can slide into
+		valid_moves = [] #all the tiles that you can slide into that dont disconnect the board
+		#find all unoccupied pieces adjacent to another piece that the piece can move to
+		for tile in adjacent_tiles(self.pos):
+			if (tile not in pieces) : #if the tile is not occupied and hasnt already been counted
+				for anchor_tile in adjacent_tiles(tile):
+					if anchor_tile == (self.pos):
+						continue
+					if (anchor_tile in pieces) and (tile not in open_tiles): #if the vacant tile is adjacent to a tile that is not the queen
+						print(tile)
+						open_tiles.append(tile)
+
+		print('All Open Tiles:')
+		print(open_tiles)
+
+		#Check to see if you can squeeze into these spots
+		
+		for tile in open_tiles:
+			if can_squeeze(self.pos,tile,pieces):
+				accessible_tiles.append(tile)
+
+		#for each potential state created by a move, evaluate wheter the board is still contiguous
+		for move in accessible_tiles:
+			state = pieces.copy()
+			state.remove(self.pos)
+			state.append(move) #If the board must stay connected while moving remove this
+			print('Possible state:')
+			print(state)
+			if is_contiguous(state):
+				valid_moves.append(move) #remove this move because it must violate the rules
+
+		return valid_moves
+
+
+if __name__ == '__main__':
+	pygame.init()
+	screen = pygame.display.set_mode(win_size)
 
 
 
-
-# class Board():
-
-# 	def __init__(self, screen):
-# 		self.screen = screen
-# 		self.player = 1
-# 		self.turn = 1
-# 		self.pieces = {}
-# 		self.piece_selected = None
-# 		self.mode = 'add'
-
-# 	def draw(self,mousepos):
-# 		if turn == 1:
-# 			Empty_hex(0, 0, mousepos, screen).draw()
-# 		else:
-# 			for piece in self.pieces:
-# 				piece.draw()
-
-# 	def add(self, player, piece_type, xpos, ypos):
+	player = Queen(1,(1,0),screen)
 
 
+	state = [(0,1),
+			 (1,-1),
+			 (0,-1)]
+
+	state.append(player.pos)
+	obstacles = []
+	for tile in state:
+		obstacles.append(Hex(tile,screen))
+
+	moves = player.valid_moves(state)
+	options = []
+	for move in moves:
+		temp = Hex(move,screen)
+		temp.color = (0,255,0)
+		options.append(temp)
 
 
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
 
-
-pygame.init()
-screen = pygame.display.set_mode(win_size)
-
-test = Queen(1,0,0,screen)
-test2 = Hex(1,0,screen)
-test3 = Queen(-1,1,1,screen)
-
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-
-
-	screen.fill((250,250,250))
-	test.draw()
-	test2.draw()
-	test3.draw()
-	pygame.display.flip()
+		screen.fill((250,250,250))
+		
+		for obstacle in obstacles:
+			obstacle.draw()
+		player.draw()
+		for option in options:
+			option.draw()
+		pygame.display.flip()
---
+++ contiguous_search_vis.py
@@ -0,0 +1,78 @@
+from Hive import *
+import pygame
+import numpy
+import time
+
+
+win_size = (1080,720)
+pygame.init()
+screen = pygame.display.set_mode(win_size)
+
+def draw_everything(Hexs, screen):
+	screen.fill((255,255,255))
+	for Hex in Hexs:
+		Hexs[Hex].draw()
+	pygame.display.flip()
+
+def highlight_hexes(Hexs, searching, checked):
+	for Hex in Hexs:
+		Hexs[Hex].color = (255,0,0)
+
+	for tile in checked:
+		Hexs[tile].color = (0,255,0)
+
+	for tile in searching:
+		Hexs[tile].color = (0,0,255)
+	return Hexs
+
+
+state = [(0,0),
+		 (1,0),
+		 (2,0),
+		 (1,-1),
+		 (2,-1),
+		 (2,-2),
+		 (1,-2),
+		 (0,-2),
+		 (0,-1),
+		 (-1,0),
+		 (-2,0),
+		 (-2,1),
+		 (-1,1),
+		 (0,1)]
+
+Hexs = {}
+for tile in state:
+	temp = Hex(tile,screen)
+	temp.color = (255,0,0)
+	Hexs[tile] = temp
+
+draw_everything(Hexs,screen)
+time.sleep(1)
+
+seed = state[0] #random starting tile
+contiguous_tiles = [seed] #a list of all tiles that are touching the starting tile
+new_tiles = [] #tiles on the edge of the search
+
+Hexs[seed].color = (0,255,0)
+draw_everything(Hexs, screen)
+time.sleep(1)
+
+for tile in adjacent_tiles(seed):
+	if tile in state: #for every existing tile adjacent to the seed
+		new_tiles.append(tile) #add the adjacent tile to the search
+		contiguous_tiles.append(tile) #add the adjacent tile to the list of touching tiles
+Hexs = highlight_hexes(Hexs, new_tiles, contiguous_tiles)
+draw_everything(Hexs, screen)
+time.sleep(1)
+
+while len(new_tiles) != 0: #until there are no tiles left to check
+	for tile in new_tiles: #for each tile to check
+		for adj_tile in adjacent_tiles(tile): #check each tile adjacent to it
+			if (adj_tile in state) and (adj_tile not in contiguous_tiles): #if it is filled and not already counted
+				new_tiles.append(adj_tile) #add it to the search pile
+				contiguous_tiles.append(adj_tile) #add it to the total touching tiles
+		new_tiles.remove(tile) #Now that it is searched remove this tile from the search pool
+		Hexs = highlight_hexes(Hexs, new_tiles, contiguous_tiles)
+		draw_everything(Hexs, screen)
+		time.sleep(1)
